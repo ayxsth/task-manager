70c355699fce917833dac3b012d196a72674008f
Aayush <ayxsth@gmail.com>
2021-09-27 17:11:37 +0545

    authenticated user and added avatar in user's profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,7 +5,6 @@ const auth = require('../middleware/auth');
 
 const router = express.Router();
 const upload = new multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000   //1 megabyte
     }, fileFilter(req, file, callback) {
@@ -92,7 +91,9 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 });
 
-router.post('/users/me/avatar', upload.single('avatar'), async (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
 }, (error, req, res, next) => {     //this function only runs when there is any error
     res.status(400).send({ error: error.message });
