404ad540ce76009c0da7ce9e53953436ef66824a
Aayush <ayxsth@gmail.com>
2021-09-28 16:13:11 +0545

    changed the format and resized the avatar before saving it in the db

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
@@ -92,7 +93,8 @@ router.delete('/users/me', auth, async (req, res) => {
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer).png().resize({ width: 250, height: 250 }).toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {     //this function only runs when there is any error
@@ -113,7 +115,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error();
         }
 
-        res.set('Content-Type', 'image/jpg');
+        res.set('Content-Type', 'image/png');
         res.send(user.avatar);
     } catch (e) {
         res.status(404).send();
