2450dde804991d43c15719005edd43890dd9cc62
Aayush <ayxsth@gmail.com>
2021-10-03 19:03:41 +0545

    added some more test suite for user

--- tests/user.test.js
+++ tests/user.test.js
@@ -94,3 +94,33 @@ test("Should delete account for user", async () => {
 test("Should not delete account for unauthorized user", async () => {
     await request(app).delete("/users/me").send().expect(401);
 });
+
+test("Should upload an avatar", async () => {
+    await request(app)
+        .post("/users/me/avatar")
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .attach("avatar", "tests/fixtures/profile-pic.jpg")
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update valid user fields", async () => {
+    await request(app)
+        .patch("/users/me")
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send({ name: "Aayush Shrestha" })
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toBe("Aayush Shrestha");
+});
+
+test("Should not update invalid user fields", async () => {
+    await request(app)
+        .patch("/users/me")
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send({ location: "Kathmandu" })
+        .expect(400);
+});
